56e1ef101b1bd8742d2d3005b24530bc40663ae6
heireev <muhammadrifqi216@gmail.com>
2023-06-04 05:59:25 +0800

    [SETUP UI - ON PROGRESS] fungsikan fitur track location + Perbaikan tema

--- app/src/main/java/com/reev/telokkaapps/ui/dashboard/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/reev/telokkaapps/ui/dashboard/fragment/home/HomeFragment.kt
@@ -1,12 +1,19 @@
 package com.reev.telokkaapps.ui.dashboard.fragment.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.reev.telokkaapps.R
 import com.reev.telokkaapps.data.source.local.dummy.dummycategory.CategoryDataSource
 import com.reev.telokkaapps.data.source.local.dummy.dummyplace.DummyPlacesData
@@ -19,6 +26,15 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    // variable untuk getCurLocation
+    private var latitude: Double? = null
+    private var longitude: Double? = null
+    private lateinit var fusedLocation: FusedLocationProviderClient
+
+    companion object {
+        private const val CUR_LOC_PERMISSION_REQUEST_CODE = 1001 // untuk getCurLocation
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +46,43 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Untuk track posisi user saat ini
+        fusedLocation = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        binding.layoutHomeFragment.minimapLayout.btnUpdateLocation.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    CUR_LOC_PERMISSION_REQUEST_CODE
+                )
+            } else {
+                fusedLocation.lastLocation.addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        latitude = location.latitude
+                        longitude = location.longitude
+                        val locationText = StringBuilder().apply {
+                            append(latitude)
+                            append(", ")
+                            append(longitude)
+                        }.toString()
+                        binding.layoutHomeFragment.minimapLayout.curLocationTV.text = locationText
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.location_not_found),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
 
 
         // untuk list kategori
