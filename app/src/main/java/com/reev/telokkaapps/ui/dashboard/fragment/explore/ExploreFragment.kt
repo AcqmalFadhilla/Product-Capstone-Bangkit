17dd8ea373bff1aee6b07fa9ed5cefaad25a2378
heireev <muhammadrifqi216@gmail.com>
2023-06-06 15:21:06 +0800

    [SETUP UI - ON PROGRESS] Set layout untuk ExploreFragment.kt

--- app/src/main/java/com/reev/telokkaapps/ui/dashboard/fragment/explore/ExploreFragment.kt
+++ app/src/main/java/com/reev/telokkaapps/ui/dashboard/fragment/explore/ExploreFragment.kt
@@ -1,16 +1,21 @@
 package com.reev.telokkaapps.ui.dashboard.fragment.explore
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.reev.telokkaapps.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.reev.telokkaapps.data.source.local.dummy.dummyplace.DummyPlacesData
+import com.reev.telokkaapps.data.source.local.dummy.dummyplace.Place
 import com.reev.telokkaapps.databinding.FragmentExploreBinding
-import com.reev.telokkaapps.databinding.FragmentHomeBinding
+import com.reev.telokkaapps.ui.dashboard.fragment.explore.adapter.SearchItemListAdapter
+import com.reev.telokkaapps.ui.detail.DetailActivity
 
 
-class ExploreFragment : Fragment() {
+class ExploreFragment : Fragment(),
+    SearchItemListAdapter.OnPlaceItemClickListener{
     private lateinit var binding: FragmentExploreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +25,29 @@ class ExploreFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // untuk item list
+        binding.listSearchPlaceLayout.sectionTitle.text = "Hasil Pencarian"
+
+        val dummyPlace = DummyPlacesData.dummyPlaces
+        val placeListAdapter = SearchItemListAdapter(dummyPlace, this)
+
+        binding.listSearchPlaceLayout.itemRecyclerView.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = placeListAdapter
+        }
+    }
+
+    override fun onPlaceItemClick(place: Place) {
+        val intent = Intent(requireContext(), DetailActivity::class.java)
+        intent.putExtra("PLACE_EXTRA", place)
+        startActivity(intent)
     }
 
 }
